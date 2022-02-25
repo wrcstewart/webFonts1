91a1fb597d57b133a0c22681a3933601be03356e
william stewart <wrcstewart@yahoo.co.uk>
2022-02-25 16:00:28 +0000

    transfer midisave function and remove global mwStr

--- midiFileSave.js
+++ midiFileSave.js
@@ -1,7 +1,9 @@
  var isCreatingFile = true;//when true writing midi file
     var tracks = [];//used for midi file writing
     var ticksPerSec = 256;//used for midi file writing
-    var mwStr;//string for saving midi file data.
+
+
+
 
 
 function addNote(track, time, pitch, duration, vel, channel) { // used for midi file writing
@@ -27,4 +29,27 @@ function addNote(track, time, pitch, duration, vel, channel) { // used for midi 
       // if instrumentMidiNum is the negative signal percussionMidiNum then select percussion channel
       if (instrumentMidiNum == percussionMidiNum) return (percussionChannel);
       return (defaultChannel)
+    }
+
+      function saveMidiFile() {
+
+      let filename = "MettatreeMusic.mid";
+      var write = new MidiWriter.Writer(tracks);
+
+      let mwStr = write.dataUri();
+      var link = document.createElement("a");
+      if (link.download !== undefined) { // feature detection
+        // Browsers that support HTML5 download attribute
+        //var url = URL.createObjectURL(blob);
+        //var url = URL.createObjectURL(mwStr);
+        var url = mwStr;
+        link.setAttribute("href", url);
+        link.setAttribute("download", filename);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+
+      //saveJSON("testMidiP9.mid", mwStr)
     }

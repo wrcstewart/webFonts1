e2526f05429cd51fb78e316c4e655112b4161df4
william stewart <wrcstewart@yahoo.co.uk>
2022-02-25 16:31:54 +0000

    transfer saveJSON to own file

---
+++ saveJSON.js
@@ -0,0 +1,24 @@
+not used at present 25/02/2022
+
+function saveJSON(filename, obj2) {
+
+      var jsonString = JSON.stringify(obj2);
+      var blob = new Blob([jsonString], {type: 'text/plain'});
+
+      if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+      } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          var url = URL.createObjectURL(blob);
+
+          link.setAttribute("href", url);
+          link.setAttribute("download", filename);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }
+      }
+    }

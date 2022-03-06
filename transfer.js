e2bdb54b6bb88e774dba4393b599eb31d3c5ed26
william stewart <wrcstewart@yahoo.co.uk>
2022-03-06 21:05:29 +0000

    X devel of chord buttons 1

--- transfer.js
+++ transfer.js
@@ -11,3 +11,17 @@ function initC(idStr,colStr,l,w,t,h) {
                     "height":h+"%"
             })
 };
+
+function crBut(str){
+
+  var $buts = [];
+
+  $buts[0] = $('<button/>',{text:'aaaaa'});
+
+$("#"+"editButtonsPanel").append($buts[0]);
+
+$buts[0].css({ "background-color":"Blue"})
+
+//initC('but1','Red',50,100,2,50);
+
+}
